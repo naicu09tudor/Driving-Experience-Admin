8c3cb00c56a6cd52cf20bbd4998f06f6dab947c7
naicu09tudor <naicu.tudor09@gmail.com>
2023-05-26 12:10:30 +0300

    Added OperationUtility class for Testing

--- src/main/java/com/drivingexperience/admin/entity/User.java
+++ src/main/java/com/drivingexperience/admin/entity/User.java
@@ -61,7 +61,6 @@ public class User {
         return "User{" +
                 "userId=" + userId +
                 ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
                 '}';
     }
 
